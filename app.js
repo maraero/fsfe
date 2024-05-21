5ea272ccd1cb7e2f668267da44ce370771cec7c0
Evgeny Kudryavtsev <themaraero@gmail.com>
2024-05-21 21:42:14 +0000

    add app

---
+++ app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+	res.write('On the way to be a full stack engineer');
+	res.end();
+}).listen(3000);
+console.log("Server started on port 3000");
