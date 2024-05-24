c1087c40711ffb01189598d96a234756724ab06c
Evgeny Kudryavtsev <themaraero@gmail.com>
2024-05-24 21:43:46 +0300

    add ws

---
+++ index-ws.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const server = require("http").createServer();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+server.on("request", app);
+server.listen(3000, () => {
+  console.log("server started on port 3000");
+});
+
+const WebSocketServer = require("ws").Server;
+const wss = new WebSocketServer({ server });
+wss.on("connection", (ws) => {
+  const numClients = wss.clients.size;
+  console.log("Clients connected", numClients);
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to my server");
+  }
+
+  ws.on("close", () => {
+    wss.broadcast(`Current visitors: ${numClients}`);
+    console.log("A client has disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach((client) => {
+    client.send(data);
+  });
+};
